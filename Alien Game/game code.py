bfebe1cd44c8993332aa437c0558be05f4e71d5d
Aaravs-GH-Account <aaravagarwal1014@gmail.com>
2026-03-19 20:38:20 -0700

    Update game code.py

--- Alien Game/game code.py
+++ Alien Game/game code.py
@@ -8,16 +8,24 @@ c=1
 def am():
     a.x=r.randint(125,375)
     a.y=r.randint(150,375)
+    clock.schedule_unique(ma,1)
 def draw():
     screen.fill('white')
     a.draw()
     screen.draw.text(b,center=(250,75),fontsize=25,color='blue')
+def ma():
+    global b
+    b='Oh No! You missed!'
+    am()
 def on_mouse_down(pos):
     global b,c
     if a.collidepoint(pos):
         b='Good Job! You hit the alien!'
+        clock.unschedule(am)
+        clock.unschedule(ma)
         am()
-        c=1
+        clock.schedule_interval(am,c)
 am()
-clock.schedule_interval(am,c)
+clock.schedule(am,3)
+#clock.schedule_interval(am,c)
 p.go()

ac6f4680c3e65243ead0fb168f54e9115133b1fd
Aaravs-GH-Account <aaravagarwal1014@gmail.com>
2026-06-16 19:54:02 -0700

    Add files via upload

--- Recycle Paper Bags/main.py
+++ Recycle Paper Bags/main.py
@@ -55,4 +55,60 @@ def make_items(number_of_extra_items):
     return new_items
 
 
+def get_option_create(number_of_extra_items):
+    items_to_create=['paper']
+    for i in range(0,number_of_extra_items):
+        random_option=random.choice(ITEMS)
+        items_to_create.append(random_option)
+    return items_to_create
+
+def create_items(items_to_create):
+    new_items=[]
+    for option in items_to_create:
+        item = Actor(option+'img')
+        new_items.append(item)
+    return new_items
+
+def layout_items(new_items):
+    gaps=len(new_items)+1
+    gap_size=WIDTH/gaps
+    random.shuffle(new_items)
+    for index, item in enumerate(new_items):
+        new_x_pos=(index+1) * gap_size
+        item.x = new_x_pos
+
+def animate_items(new_items):
+    global animations
+    for i in new_items:
+        duration= START_SPEED-current_level
+        animation=animate(i, duration=duration, on_finished=handle_game_over, y=HEIGHT)
+        animations.append(animation)
+        
+def handle_game_over():
+    global game_over
+    game_over=True
+
+def on_mouse_down(pos):
+    global items
+    for item in items:
+        if item.collidepoint(pos):
+            if 'paper' in item.image:
+                handle_game_complete()
+            else:
+                handle_game_over()
+
+def handle_game_complete():
+    global current_level, items, animations, game_complete
+    stop_animations(animations)
+    if current_level==FINAL_LEVEL:
+        game_complete=True
+    else:
+        current_level=current_level+1
+        items=[]
+        animations=[]
+
+def stop_animations(animations):
+    for i in animations:
+        if i.running:
+            i.stop()
 p.go()
